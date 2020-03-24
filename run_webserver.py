b4e9b904718922100c871eeb3a59f7c5389a8d99
ken-sho <gakto@mail.ru>
2020-03-24 18:33:52 +0300

    release_240320b1

--- run_webserver.py
+++ run_webserver.py
@@ -471,7 +471,7 @@ if __name__ == "__main__":
     print ("WEB server Running...")
     print ("Press ctrl + C to close")
     #http_server = tornado.httpserver.HTTPServer(application,xheaders=True, ssl_options={'certfile': 'ssl/tornado.crt', 'keyfile': 'ssl/tornado.key'})
-    http_server = tornado.httpserver.HTTPServer(application, ssl_options={'certfile': 'ssl/tornado.crt', 'keyfile': 'ssl/tornado.key'})
+    http_server = tornado.httpserver.HTTPServer(application, ssl_options={'certfile': 'opt/IORDan/ssl/tornado.crt', 'keyfile': 'opt/IORDan/ssl/tornado.key'})
     http_server.listen(443)
     #http_server = tornado.httpserver.HTTPServer(application)
     #http_server.listen(8082)
