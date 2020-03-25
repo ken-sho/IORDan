728c71164581bec8ce7d640b2a06c10f17f0bd4a
ken-sho <gakto@mail.ru>
2020-03-26 02:05:39 +0300

    release250320b6

--- modules/unloadxls.py
+++ modules/unloadxls.py
@@ -7,5 +7,5 @@ def UnloadFile(html_body,file_name):
     table = pd.read_html(html_body,header=0)[0]
     timestamp = int(time.time())
     table.to_excel("/download/"+file_name+str(timestamp)+".xlsx",sheet_name='report')
-    res='/opt/IORDan/download/'+file_name+str(timestamp)+'.xlsx'
+    res='/download/'+file_name+str(timestamp)+'.xlsx'
     return res
