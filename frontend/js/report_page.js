6a5792f80b576b58aa243482d95bf732d264b8a0
ken-sho <gakto@mail.ru>
2020-07-09 12:06:58 +0300

    release07072020b3

--- frontend/js/report_page.js
+++ frontend/js/report_page.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
 
         var printWindow = window.open('', 'PRINT');
         let printingContent = document.querySelector('#report_content').innerHTML;
-        printWindow.document.write(`<html><head><script src="/js/jquery-3.4.1.min.js"></script><script src="/js/print_report_page.js"></script><style type="text/css">body, table {font-size: ${fontSize}px;} table {border-collapse: collapse;} .report-table th, .report-table td {border: 1px solid black;}</style>`);
+        printWindow.document.write(`<html><head><script src="/js/jquery-3.4.1.min.js"></script><script src="/js/print_report_page.js"></script><style type="text/css">body, table {font-size: ${fontSize}px;} .report-table {border-collapse: collapse; width: 100%; page-break-after: always} .report-table th, .report-table td {border: 1px solid black;}</style>`);
         printWindow.document.write('</head><body id="report_print">');
         printWindow.document.write(printingContent);
         printWindow.document.write('</body></html>');
