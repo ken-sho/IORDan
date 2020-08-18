b0911e9d7bc4f81d812cf6e927c30165fb3f99cd
ken-sho <gakto@mail.ru>
2020-08-19 01:25:25 +0300

    190820b

--- frontend/js/print_report_page.js
+++ frontend/js/print_report_page.js
@@ -1,13 +1,15 @@
-$(document).ready(function () {
-
+$(document).ready(function () {    
     let printNotation = sessionStorage.getItem('printNotation');
+    const fontSize = sessionStorage.getItem('report_print_font_size');
 
-    if (printNotation !== '') {
+    $('body, table').css({'font-size': `${fontSize}px`})
+
+    if (printNotation !== '' && printNotation !== 'null') {
         let tableFooter = `<tfoot><tr><td><div class="report-print-footer">${printNotation}</div><div class="page-footer-space"></div></td></tr></tfoot>`
         $('#report_print').html(`<table id="printing_content"><tbody><tr><td>${$('#report_print').html()}</td></tr></tbody>${tableFooter}</table>`);
     }
 
-    window.print();
-    setTimeout(function () { window.close(); }, 10);
+    // window.print();
+    // setTimeout(function () { window.close(); }, 10);
 
 });
--- frontend/js/report_page.js
+++ frontend/js/report_page.js
@@ -1,9 +1,11 @@
 $(document).ready(function () {
 
     let fontSize = $('#font_size_select').val();
+    sessionStorage.setItem('report_print_font_size', fontSize);
 
     $('#font_size_select').on('change', function() {
         fontSize = $(this).val();
+        sessionStorage.setItem('report_print_font_size', fontSize);
         $('#report_content, #report_content table').css({'font-size':`${fontSize}px`});
     });
 
@@ -11,7 +13,7 @@ $(document).ready(function () {
 
         var printWindow = window.open('', 'PRINT');
         let printingContent = document.querySelector('#report_content').innerHTML;
-        printWindow.document.write(`<html><head><script src="/js/jquery-3.4.1.min.js"></script><script src="/js/print_report_page.js"></script><style type="text/css">body, table {font-size: ${fontSize}px;} .report-table {border-collapse: collapse; width: 100%; page-break-after: always} .report-table th, .report-table td {border: 1px solid black;}</style>`);
+        printWindow.document.write(`<html><head><script src="/js/jquery-3.4.1.min.js"></script><script src="/js/print_report_page.js"></script><link href="/css/style_print_report_page.css" rel="stylesheet" type="text/css">`);
         printWindow.document.write('</head><body id="report_print">');
         printWindow.document.write(printingContent);
         printWindow.document.write('</body></html>');
