23848d8c096d42c2b59cd3d110dc6a0a9d3d5bb5
ken-sho <gakto1981@gmail.com>
2021-05-04 21:10:24 +0300

    redmine

---
+++ run_webserver_re2.py
@@ -0,0 +1,27 @@
+import tornado.httpserver
+import tornado.ioloop
+import tornado.web
+
+
+class LoginHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("redirect/redirect2.html")
+
+settings = {
+    "cookie_secret": "plhfdcndeqltleirfvjhjp111",
+}
+
+application = tornado.web.Application([
+    (r"/", LoginHandler),
+], **settings)
+#])
+
+if __name__ == "__main__":
+    print ("WEB server Running...")
+    print ("Press ctrl + C to close")
+    #http_server = tornado.httpserver.HTTPServer(application,xheaders=True, ssl_options={'certfile': 'ssl/tornado.crt', 'keyfile': 'ssl/tornado.key'})
+    #http_server = tornado.httpserver.HTTPServer(application, ssl_options={'certfile': 'opt/IORDan/ssl/tornado.crt', 'keyfile': 'opt/IORDan/ssl/tornado.key'})
+    #http_server.listen(10443)
+    http_server = tornado.httpserver.HTTPServer(application)
+    http_server.listen(10443)
+    tornado.ioloop.IOLoop.instance().start()
